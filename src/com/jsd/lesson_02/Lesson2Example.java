55aa0a919473437a37a1ca474b50e6e87adc8e77
Dmitry F <dimfroloff@gmail.com>
2022-02-09 20:32:32 +0500

    primitive variables

---
+++ src/com/jsd/lesson_02/Lesson2Example.java
@@ -0,0 +1,26 @@
+package com.jsd.lesson_02;
+
+public class Lesson2Example {
+
+    public static void main(String[] args) {
+
+        int temp;
+        temp = 567;
+        System.out.println(temp);
+
+        int value = 25;
+        System.out.println(value);
+
+        double value2 = 78.123;
+        System.out.println(value2);
+
+        boolean booleanValue = true;
+        System.out.println(booleanValue);
+
+        char charValue = 'a';
+        System.out.println(charValue);
+
+        charValue = 't';
+        System.out.println(charValue);
+    }
+}
