aee8d1f53c19b9379fcea85e3c05d7c1db1b658a
Dmitry F <dimfroloff@gmail.com>
2022-02-10 14:05:31 +0500

    operations with variables

---
+++ src/com/jsd/lesson_03/BooleanExample.java
@@ -0,0 +1,13 @@
+package com.jsd.lesson_03;
+
+public class BooleanExample {
+
+    public static void main(String[] args) {
+
+        int value1 = 0;
+        int value2 = 10;
+        boolean test = value1 != 0;
+        boolean test2 = value2 > 0;
+        System.out.println(test || test2);
+    }
+}
---
+++ src/com/jsd/lesson_03/Lesson3Example.java
@@ -0,0 +1,24 @@
+package com.jsd.lesson_03;
+
+public class Lesson3Example {
+
+    public static void main(String[] args) {
+//        int value = 5;
+//        int value2 = 3;
+//        int sum = value % value2;
+//        System.out.println(sum);
+
+//        int value = 5;
+//        value += 25;
+//        System.out.println(value);
+
+        int value = 10;
+//        value = value + 1;
+//        value += 1;
+        value++;
+        System.out.println(value);
+        value--;
+        System.out.println(value);
+
+    }
+}
