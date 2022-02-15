acddf355c8d0f8075f46225efa013630b767692b
Dmitry F <dimfroloff@gmail.com>
2022-02-15 17:10:30 +0500

    if else tasks

--- src/com/jsd/lesson_04/SwitchExample.java
+++ src/com/jsd/lesson_04/SwitchExample.java
@@ -4,6 +4,22 @@ public class SwitchExample {
     public static void main(String[] args) {
 //        byte, short, int, char, String, enum
         int value = 2;
+
+        var charValue = switch (value) {
+            case 1 -> '1';
+            case 2, 3, 4 -> '2';
+            default -> '0';
+        };
+
+        switch (value) {
+            case 1 -> System.out.println("value == 1");
+            case 2, 3, 4 -> {
+                System.out.println("value == 2, 3, 4");
+                System.out.println("value == 2, 3, 4");
+            }
+            default -> System.out.println("Нет такого значения");
+        }
+
         switch (value) {
             case 1:
                 System.out.println("value == 1");
