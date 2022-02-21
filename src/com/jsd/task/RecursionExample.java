96601372f90879fc68c0e8f65c50d904610d291a
Dmitry F <dimfroloff@gmail.com>
2022-02-21 16:28:56 +0500

    for-while practice

---
+++ src/com/jsd/task/RecursionExample.java
@@ -0,0 +1,21 @@
+package com.jsd.task;
+
+/**
+ * 1. Написать программу высчитывающую факториал введённого целого числа.
+ */
+
+public class RecursionExample {
+
+    public static void main(String[] args) {
+        int factorial = factorial(5);
+        System.out.println(factorial);
+    }
+
+    public static int factorial(int value) {
+        if (value == 1) {
+            return 1;
+        }
+
+        return value * factorial(value -1 );
+    }
+}
