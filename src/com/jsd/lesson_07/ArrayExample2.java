1a3ba05cd6e70f0165995e4c00bcad5039389987
Dmitry F <dimfroloff@gmail.com>
2022-02-24 18:51:25 +0500

    2d arrays

---
+++ src/com/jsd/lesson_07/ArrayExample2.java
@@ -0,0 +1,29 @@
+package com.jsd.lesson_07;
+
+public class ArrayExample2 {
+
+    public static void main(String[] args) {
+        int[][] values2 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        int[][] values = new int[3][];
+        values[0] = new int[5];
+        values[1] = new int[2];
+        values[2] = new int[1];
+
+        printArray(values);
+    }
+
+    private static void printArray(int[][] values2) {
+        for (int i = 0; i < values2.length; i++) {
+            int[] ints = values2[i];
+            for (int j = 0; j < ints.length; j++) {
+                System.out.print(ints[j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
