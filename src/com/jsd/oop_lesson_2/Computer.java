16a6dcf1406a46eaf1e0a8101c851883d2a3bc55
Dmitry F <dimfroloff@gmail.com>
2022-03-02 14:26:13 +0500

    constructor

--- src/com/jsd/oop_lesson_2/Computer.java
+++ src/com/jsd/oop_lesson_2/Computer.java
@@ -5,6 +5,15 @@ public class Computer {
     int ssd = 500;
     int ram = 1024;
 
+    Computer() {
+        System.out.println("Я был создан");
+    }
+
+    public Computer(int ssd, int ram) {
+        this.ssd = ssd;
+        this.ram = ram;
+    }
+
     void load() {
         System.out.println("Я загрузился");
     }
--- src/com/jsd/oop_lesson_2/ComputerRunner.java
+++ src/com/jsd/oop_lesson_2/ComputerRunner.java
@@ -5,8 +5,11 @@ public class ComputerRunner {
     public static void main(String[] args) {
         int value = 5;
         Computer computer = new Computer();
-        computer.load();
-        System.out.println("Ram: " + computer.ram);
-        System.out.println("Ssd: " + computer.ssd);
+        computer.printState();
+//        System.out.println("Ram: " + computer.ram);
+//        System.out.println("Ssd: " + computer.ssd);
+
+        Computer computer2 = new Computer(128, 16);
+        computer2.printState();
     }
 }
