f25a6d1df869dc02476e4218249823497567fe55
Dmitry F <dimfroloff@gmail.com>
2022-03-15 22:03:24 +0500

    instanceof

--- src/com/jsd/oop_lesson_10/PolyLessonRunner.java
+++ src/com/jsd/oop_lesson_10/PolyLessonRunner.java
@@ -7,14 +7,20 @@ public class PolyLessonRunner {
 //        laptop.load();
 //        System.out.println();
         Computer mobile = new Mobile(new Ssd(250), new Ram(1024));
+
+        Laptop laptop1 = new Laptop(new Ssd(5000), new Ram(10000), 5);
 //        mobile.load();
-        loadComputers(laptop, mobile);
-        printInfo(new Computer[]{laptop, mobile});
+//        loadComputers(laptop, mobile);
+        printInfo(new Computer[]{laptop, mobile, laptop1});
     }
 
     public static void printInfo(Computer[] computers) {
         for (Computer computer : computers) {
             computer.print();
+            if (computer instanceof Laptop) {
+                Laptop laptop = (Laptop) computer;
+                laptop.open();
+            }
             System.out.println();
         }
     }
