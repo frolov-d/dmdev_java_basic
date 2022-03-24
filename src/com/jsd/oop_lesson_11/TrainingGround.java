914da45681c7b405885c791e7c2df00bd4eea929
Dmitry F <dimfroloff@gmail.com>
2022-03-24 12:56:09 +0500

    inner, nested classes

--- src/com/jsd/oop_lesson_11/TrainingGround.java
+++ src/com/jsd/oop_lesson_11/TrainingGround.java
@@ -5,7 +5,9 @@ public class TrainingGround {
     public static void main(String[] args) {
         Hero warrior = new Warrior("Боромир", 15);
         Hero mage = new Mage("Гендальф", 20);
-        Hero archer = new Archer("Леголас", 10);
+        Archer archer = new Archer("Леголас", 10);
+
+        Archer.Wolf second_wolf = archer.new Wolf("Second wolf", 11);
 
         Enemy enemy = new Enemy("Зомби", 100);
 
